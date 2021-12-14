fae6af646e35ef248c8ec52e6676bd9886db697c
sergio2305 <sergio230564@gmail.com>
2021-12-13 22:59:56 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscliente/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dscliente/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscliente.dto.ClientDTO;
 import com.devsuperior.dscliente.entities.Client;
 import com.devsuperior.dscliente.repositories.ClientRepository;
+import com.devsuperior.dscliente.services.exceptions.DatabaseException;
 import com.devsuperior.dscliente.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -67,6 +70,18 @@ public class ClientService {
 			
 		}
 		
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 }
 
